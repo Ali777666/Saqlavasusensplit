4fd68a58d4672c210d01dbbf87f488c8b5a02fe8
Ali777666 <84578373+Ali777666@users.noreply.github.com>
2024-06-20 23:26:50 +0500

    Update batch.py

--- main/plugins/batch.py
+++ main/plugins/batch.py
@@ -5,7 +5,7 @@
 Plugin for both public & private channels!
 """
 
-import time, os, asyncio, decouple
+import time, os, asyncio, decouple,random
 
 from .. import bot as Drone
 from .. import userbot, Bot, AUTH
